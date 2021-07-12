8f50d6afc2b7e4e20244e88e2ba8dcb3ff708a94
Zero <node.zerocool@gmail.com>
2021-07-12 18:42:43 -0500

    markup

--- app.py
+++ app.py
@@ -17,3 +17,17 @@ VERIFY_SERVICE_SID= os.environ.get('VERIFY_SERVICE_SID')
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 KNOWN_PARTICIPANTS = app.config['KNOWN_PARTICIPANTS']
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        if username in KNOWN_PARTICIPANTS:
+            session['username'] = username
+            send_verification(username)
+            return redirect(url_for('verify_passcode_input'))
+        error = "User not found. Please try again."
+        return render_template('index.html', error = error)
+    return render_template('index.html')
