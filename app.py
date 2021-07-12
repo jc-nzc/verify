8e0a736ae896571f1a2c19fda0114e20c45c679a
Zero <node.zerocool@gmail.com>
2021-07-12 18:40:33 -0500

    settings

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+from flask import Flask, request, render_template, redirect, session, url_for
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
+
+load_dotenv()
+app = Flask(__name__)
+app.secret_key = 'secretkeyfordungeon'
+app.config.from_object('settings')
+
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN= os.environ.get('TWILIO_AUTH_TOKEN')
+VERIFY_SERVICE_SID= os.environ.get('VERIFY_SERVICE_SID')
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+KNOWN_PARTICIPANTS = app.config['KNOWN_PARTICIPANTS']
---
+++ settings.py
@@ -0,0 +1,5 @@
+KNOWN_PARTICIPANTS = {
+  'herooftime@hyrule.com':  '<YOUR_PHONE_NUMBER> ',
+  'zelda@hyrule.com': '+15552211986',
+  'tetra@hyrule.com': '+15553242003'
+}
